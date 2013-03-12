17839e12c069aa318a047046ded4a9f17d85f7a5
丁志鹏 <514591280@qq.com>
2013-03-12 09:33:58 +0800

    增加了对Converter.cs的注释说明
    缺乏写代码注释的习惯勿怪，现在好歹也要养成这些习惯了

--- PicToAscii/Converter.cs
+++ PicToAscii/Converter.cs
@@ -5,20 +5,28 @@ using System.Linq;
 
 namespace PicToAscii
 {
+    /// <summary>
+    /// 转换器
+    /// </summary>
     internal class Converter
     {
         private Bitmap bmp;
-
+        /// <summary>
+        /// 设置或获取需要转换的图片
+        /// </summary>
         public Bitmap Bmp
         {
             get { return bmp; }
             set { bmp = value; }
         }
-
+        
         private Color conCol;
         private Color tmpCol;
+       
         private int precision;
-
+        /// <summary>
+        /// 设置或获取输出的精度
+        /// </summary>
         public int Precision
         {
             get { return precision; }
@@ -28,9 +36,11 @@ namespace PicToAscii
                     precision = value;
             }
         }
-
+        
         private bool colour;
-
+        /// <summary>
+        /// 设置是否输出为彩色（暂无用）
+        /// </summary>
         public bool Colour
         {
             get { return colour; }
@@ -38,17 +48,23 @@ namespace PicToAscii
         }
 
         private int scale;
-
+        /// <summary>
+        /// 设置或获取缩放比例
+        /// </summary>
         public int Scale
         {
             get { return scale; }
             set { scale = value; }
         }
 
+
         //MN#$@QHKER&Bk%DW68F9gh50ZXUOAdPGpS2IJbLmfVq4TCYny3u[l*]xw7avs{}ej|r1+z?/o()=tci\\<>!_\"-~^;,':`.
         private static string characters = "MN#$@QEHKRB&D%Wk896FA0U5XZghOdPSGpL4bIfTJVm2q3nYCy[]" +
-            "lu*awx7sev{|}r?zj+1)/(=tco><\\i!_\"~-^,;':.`  ";
-
+            "lu*awx7sev{|}r?zj+1)/(=tco><\\i!_\"~-^,;':.` ";
+        /// <summary>
+        /// 创建一个转换器
+        /// </summary>
+        /// <param name="bmp">需转换的Bitmap对象</param>
         public Converter(Bitmap bmp)
         {
             Bmp = bmp;
@@ -56,7 +72,11 @@ namespace PicToAscii
             Colour = true;
             Scale = 1;
         }
-
+        /// <summary>
+        /// 输出为txt文件
+        /// </summary>
+        /// <param name="txt">输出的txt文件的完整路径</param>
+        
         public void writeTxt(string txt)
         {
             int stepW = Precision;
@@ -74,7 +94,10 @@ namespace PicToAscii
             }
             sw.Close();
         }
-
+        /// <summary>
+        /// 输出为html文件
+        /// </summary>
+        /// <param name="html">输出的html文件的完整路径</param>
         public void writeHtml(string html)
         {
             StreamWriter sw = new StreamWriter(html);
@@ -83,15 +106,21 @@ namespace PicToAscii
             writeHtmlFoot(sw);
             sw.Close();
         }
-
+        /// <summary>
+        /// 写入html的头部
+        /// </summary>
+        /// <param name="sw">输出流</param>
         private void writeHtmlHead(StreamWriter sw)
         {
             if (sw != null)
             {
-                sw.WriteLine("<html><head><style type=\"text/css\">pre { font-size:2px; line-height:1; }</style></head><body>");
+                sw.WriteLine("<html><head><style type=\"text/css\">pre { font-size:1px; line-height:1; }</style></head><body>");
             }
         }
-
+        /// <summary>
+        /// 写入html的Body部分
+        /// </summary>
+        /// <param name="sw">输出流</param>
         private void writeHtmlBody(StreamWriter sw)
         {
             if (sw != null)
@@ -129,7 +158,10 @@ namespace PicToAscii
                 sw.WriteLine("</c></pre>");
             }
         }
-
+        /// <summary>
+        /// 写入html的尾部
+        /// </summary>
+        /// <param name="sw">输出流</param>
         private void writeHtmlFoot(StreamWriter sw)
         {
             if (sw != null)
@@ -137,7 +169,10 @@ namespace PicToAscii
                 sw.WriteLine("</body></html>");
             }
         }
-
+        /// <summary>
+        /// 输出为bmp文件
+        /// </summary>
+        /// <param name="pic">输出的bmp文件的完整路径</param>
         public void writePic(string pic)
         {
             int stepW = Precision ;
@@ -157,7 +192,15 @@ namespace PicToAscii
             }
             newBmp.Save(pic);
         }
-
+        /// <summary>
+        /// 获取图片的一个区域的平均颜色
+        /// </summary>
+        /// <param name="bitmap">源图片</param>
+        /// <param name="x">区域x方向起始像素</param>
+        /// <param name="y">区域y方向起始像素</param>
+        /// <param name="w">区域的宽度</param>
+        /// <param name="h">区域的高度</param>
+        /// <returns></returns>
         private Color getColor(Bitmap bitmap,int x, int y, int w, int h)
         {
             int r, g, b;
